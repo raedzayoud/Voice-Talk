382af14c7cfb674125032656bdc340fb71a323d2
raedzayoud <raed.zayoud.23@gmail.com>
2025-08-12 10:42:16 +0200

    terminate login and signup design

--- src/app/singup/singup.ts
+++ src/app/singup/singup.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-singup',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './singup.html',
-  styleUrl: './singup.scss'
+  styleUrl: './singup.scss',
 })
-export class Singup {
-
-}
+export class Singup {}
