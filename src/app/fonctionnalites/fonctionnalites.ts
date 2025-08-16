2e63c70edccc8603bcfe0e32e517fea0c98ea498
raedzayoud <raed.zayoud.23@gmail.com>
2025-08-16 11:51:41 +0200

    end of design page fonctionalite

--- src/app/fonctionnalites/fonctionnalites.ts
+++ src/app/fonctionnalites/fonctionnalites.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Menu } from '../menu/menu';
+import { Footer } from '../component/footer/footer';
 
 @Component({
   selector: 'app-fonctionnalites',
-  imports: [Menu],
+  imports: [Menu, Footer],
   templateUrl: './fonctionnalites.html',
   styleUrl: './fonctionnalites.scss',
 })
